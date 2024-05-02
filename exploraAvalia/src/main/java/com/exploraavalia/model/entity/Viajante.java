945c0e0dfc2716a5291868e06960be56b7e6a12c
AUGUSTO SÉRGIO DOS SANTOS COSTA <augusto.costa@GRANBERY.local>
2024-05-02 20:22:59 -0300

    Adicionar informações a classe ViajanteService e Viajante.

--- exploraAvalia/src/main/java/com/exploraavalia/model/entity/Viajante.java
+++ exploraAvalia/src/main/java/com/exploraavalia/model/entity/Viajante.java
@@ -1,4 +1,82 @@
 package com.exploraavalia.model.entity;
 
+import jakarta.persistence.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "exploraavalia")
 public class Viajante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String sobrenome;
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
+    private Date dataNascimento;
+
+    private String email;
+    private int nivelExperiencia;
+    private String telefone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getNivelExperiencia() {
+        return nivelExperiencia;
+    }
+
+    public void setNivelExperiencia(int nivelExperiencia) {
+        this.nivelExperiencia = nivelExperiencia;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+
 }
--- exploraAvalia/src/main/java/com/exploraavalia/service/ViajanteService.java
+++ exploraAvalia/src/main/java/com/exploraavalia/service/ViajanteService.java
@@ -1,4 +1,59 @@
 package com.exploraavalia.service;
 
+import com.exploraavalia.model.entity.Viajante;
+import com.exploraavalia.model.repository.ViajanteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ViajanteService {
+
+    private ViajanteRepository viajanteRepository;
+
+    public ViajanteService(ViajanteRepository viajanteRepository){
+        this.viajanteRepository = viajanteRepository;
+    }
+
+    public List<Viajante> findAll(){
+        return viajanteRepository.findAll();
+    }
+
+    public Viajante findById(Long id) throws Exception {
+
+        Optional<Viajante> viajante = viajanteRepository.findById(id);
+
+        if (!viajante.isPresent()) {
+            throw new Exception("Viajante não encontrado");
+        }
+
+        return viajante.get();
+    }
+
+    public Viajante save(Viajante viajante) throws Exception {
+
+        if (viajante.getNome() == null || viajante.getNome().length() < 3) {
+            throw new Exception("Nome deve ter pelo menos 3 caracteres.");
+        }
+
+        viajante.setDataNascimento(new Date());
+        return viajanteRepository.save(viajante);
+    }
+
+    public Viajante delete(Long id) throws Exception {
+        Optional<Viajante> viajante = viajanteRepository.findById(id);
+
+        if (!viajante.isPresent()) {
+            throw new Exception("Viajante não encontrado");
+        }
+
+        viajanteRepository.delete(viajante.get());
+        return viajante.get();
+    }
+
+        public Long count(){
+        return viajanteRepository.count();
+    }
 }
